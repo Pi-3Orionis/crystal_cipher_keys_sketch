fde2c8a462b029c9894bf6f43a7b53992286f616
Pi-3Orionis <46107640+Pi-3Orionis@users.noreply.github.com>
2024-10-01 21:24:01 -0500

    Update crystal_cipher_keys_sketch.ino
    Converted program to 'Simon' game, meaning the players have to reveal a sequence of glyphs by pressing the correct sections of the sequence as they are progressively revealed.

--- crystal_cipher_keys_sketch.ino
+++ crystal_cipher_keys_sketch.ino
@@ -15,8 +15,8 @@
 #define PIN_GPIO_U2_CS 15
 #define PIN_LED_CS 4
 
-// Glyph count, 26 letters in the alphabet.
-#define GLYPH_COUNT 26
+// Glyph count, equivalent letters A through F.
+#define GLYPH_COUNT 6
 
 // GPIO pin counts.
 #define U1_PIN_COUNT 16
@@ -27,10 +27,11 @@ The states for the program's main state machine execution loop.
 */
 enum ProgramState {
   WAITING_FOR_CRYSTAL,
-  ANNOUNCE_RIDDLE,
-  WAITING_FOR_ANSWER,
+  BEGIN_PUZZLE,
+  SHOW_SEQUENCE,
+  WAITING_FOR_PRESS,
   CRYSTAL_PULLED,
-  RIDDLE_ANSWERED
+  PUZZLE_SOLVED
 };
 
 /*
@@ -41,30 +42,37 @@ enum GlyphState {
   IDLE_DARK,
   IDLE_FLICKER_ON,
   IDLE_FLICKER_OFF,
-  WAITING_FOR_PUSH,
+  SIGNAL_FLASH,
   INCORRECT_PUSH,
   CORRECT_PUSH,
-  LOCK_SUBSEQUENT,
-  WAITING_FOR_SUBSEQUENT,
-  LOCK_COMPLETE,
-  TRANSFER_TO_CRYSTAL,
   RESET_TO_DARK
 };
 
 /*
 LED color.
 */
-typedef struct {
+struct Color {
   byte red;
   byte green;
   byte blue;
-} Color;
+};
+
+/*
+Glyph is a struct that encapsulates an entire glyph button.
+*/
+struct Glyph {
+  GlyphState state;
+  Color currentColor;
+  Color toColor;
+  byte speed;     // The delta of the largest color component change per pulse.
+  byte keyframe;  // 255 signals that the animation is complete.
+};
 
 // Answer strings.
 // Must be all uppercase, alphabetical characters only.
 const int answerCount = 1;
 const char* answers[answerCount] = {
-  "PARK"
+  "ABCDEF"
 };
 
 Adafruit_MCP23X17 gpioU1;
@@ -73,27 +81,18 @@ Adafruit_NeoPixel leds(GLYPH_COUNT, PIN_LED_CS);
 
 ProgramState currentState;
 ProgramState toState;
-char* currentAnswer;
-int currentLetter;
+
+const char* answerSequence;
+int revealedIndex;
+int showIndex;
+int pressIndex;
 
 Color dark;
 Color wrong_red;
 Color good_green;
-Color subsequent_cyan;
+Color signal_cyan;
 const byte slowSpeed = 8;
 const byte fastSpeed = 32;
-
-/*
-Glyph is a struct that encapsulates an entire glyph button.
-*/
-typedef struct {
-  GlyphState state;
-  Color currentColor;
-  Color toColor;
-  byte speed;     // The delta of the largest color component change per pulse.
-  byte keyframe;  // 255 signals that the animation is complete.
-  byte remainingLetterCount;
-} Glyph;
 
 Glyph glyphs[GLYPH_COUNT];
 
@@ -113,10 +112,10 @@ void setup() {
   good_green.green = 255;
   good_green.blue = 16;
 
-  // Subsequent Cyan.
-  subsequent_cyan.red = 8;
-  subsequent_cyan.green = 192;
-  subsequent_cyan.blue = 255;
+  // Signal Cyan.
+  signal_cyan.red = 8;
+  signal_cyan.green = 192;
+  signal_cyan.blue = 255;
 
   // Initialize program state variables.
   currentState = WAITING_FOR_CRYSTAL;
@@ -157,13 +156,16 @@ void setup() {
 
   // Initialize LED control.
   leds.begin();
+
+  // Initialize glyphs.
+  clearGlyphs();
 }
 
 void loop() {
   // Check for crystal presence, which is the key to making the puzzle active.
   if (!isCrystalPresent()) {
     // No crystal is detected as present.
-    if (currentState != WAITING_FOR_CRYSTAL) {
+    if (currentState != WAITING_FOR_CRYSTAL && currentState != CRYSTAL_PULLED) {
       // Pulling the crystal causes the puzzle to go to the "crystal pulled"
       // state, as an LED animation, which quickly reverts to "waiting for
       // crystal."
@@ -175,36 +177,71 @@ void loop() {
     // Crystal is in the IR socket.
     if (currentState == WAITING_FOR_CRYSTAL) {
       // We are detecting the crystal's insertion, so move to riddle delivery.
-      moveProgramState(ANNOUNCE_RIDDLE);
+      moveProgramState(BEGIN_PUZZLE);
     }
     // Otherwise it's safe to assume we've already cycled a crystal insertion.
     // Whatever the current state is should persist beyond this check.
   }
 
-  // Cycle through glyphs
+  // Process program state transitions.
+  int randomSequence = millis() % answerCount;
+  int glyphId = answerSequence[0] - 65;
+  if (currentState != toState) {
+    switch (toState) {
+      case BEGIN_PUZZLE:
+        // More complex intro? For now just select a random sequence and
+        // show it to players.
+        answerSequence = answers[randomSequence];
+        revealedIndex = 0;
+        moveProgramState(SHOW_SEQUENCE);
+        break;
+      case SHOW_SEQUENCE:
+        // Reset indices for tracking progress across sequence.
+        showIndex = 0;
+        pressIndex = 0;
+        // Start animation on the first glyph.
+        setColor(glyphs[glyphId], signal_cyan, fastSpeed);
+        break;
+      case CRYSTAL_PULLED:
+        // Crystal was removed prematurely.
+        // TODO: More than just turn off glyph lights?
+        clearGlyphs();
+        moveProgramState(WAITING_FOR_CRYSTAL);
+        break;
+      case PUZZLE_SOLVED:
+        // TODO: More detail? For now just flash the glyphs all green.
+        // Also, how to 'send' success to crystal?
+        flashSuccessful();
+        // The program will linger in this state until the crystal is
+        // pulled.
+    }
+    currentState = toState;
+  }
+
+  // Cycle through glyphs, and track if we're awaiting animations to finish.
+  bool animationFinished = true;
   for (int i = 0; i < GLYPH_COUNT; i++) {
-    checkGlyph(i);
+    animationFinished = animationFinished && checkGlyph(i);
   }
   leds.show();
 
-  // Check for riddle answered.
-  if (currentState == WAITING_FOR_ANSWER) {
-    if (currentAnswer[currentLetter] == '\0') {
-      // We've reached the end of the answer string, so the answer is successfully
-      // completed.
-      moveProgramState(RIDDLE_ANSWERED);
+  // If animations are still running, suspend the rest of the program logic.
+  if (!animationFinished) {
+    return;
+  }
+
+  // If we're showing the sequence and animations finished, either advance to
+  // the next revealed glyph or return to waiting for input.
+  if (currentState == SHOW_SEQUENCE) {
+    if (++showIndex > revealedIndex) {
+      // We've shown everything to reveal for now.
+      moveProgramState(WAITING_FOR_PRESS);
     } else {
-      // We're still in the midst of solving the riddle.
-      // If the next letter to guess is a subsequent lock, we need to change
-      // it to "waiting for subsequent" which visually cues the user that a
-      // previously locked letter has now unlocked and needs to be pressed.
-      int letter = currentAnswer[currentLetter] - 65;
-      Glyph glyph = glyphs[letter];
-      if (glyph.state == LOCK_SUBSEQUENT) {
-        glyph.state = WAITING_FOR_SUBSEQUENT;
-        setColor(glyph, subsequent_cyan, fastSpeed);
-      }
+      // Trigger the next glyph to show.
+      int glyphId = answerSequence[showIndex] - 65;
+      setColor(glyphs[glyphId], signal_cyan, fastSpeed);
     }
+    return;
   }
 }
 
@@ -215,91 +252,86 @@ void moveProgramState(ProgramState newState) {
   if (currentState == newState) {
     return;
   }
-
   toState = newState;
 }
 
 /*
 Run all glyph-specific checks that are iterated over the full set of glyphs.
+Returns false if the glyph is in the midst if animating.
 */
-void checkGlyph(int index) {
+bool checkGlyph(int glyphId) {
   // Fetch the Glyph instance.
-  Glyph glyph = glyphs[index];
+  Glyph &glyph = glyphs[glyphId];
   GlyphState state = glyph.state;
 
-  bool checkForPress = (currentState == WAITING_FOR_ANSWER)
-                       && (currentAnswer[currentLetter] != '\0')  // Riddle is now solved.
-                       && ((state == WAITING_FOR_PUSH)
-                           || (state == WAITING_FOR_SUBSEQUENT))
-                       && isGlyphPressed(index);
+  // If the glyph is still animating, that takes precedence.
+  if (glyph.keyframe < 255) {
+    bool finished = true;
+    finished = finished && adjustColorComponent(glyph.currentColor.red, glyph.toColor.red, glyph.speed);
+    finished = finished && adjustColorComponent(glyph.currentColor.green, glyph.toColor.green, glyph.speed);
+    finished = finished && adjustColorComponent(glyph.currentColor.blue, glyph.toColor.blue, glyph.speed);
 
-  if (checkForPress) {
-    // Get the index for the current letter of the riddle.
-    int letter = currentAnswer[currentLetter] - 65;
+    if (finished || ((255 - glyph.keyframe) < glyph.speed)) {
+      // Mark the keyframe as finished.
+      glyph.keyframe = 255;
+    } else {
+      // If not finished, advance keyframe and return false.
+      glyph.keyframe += glyph.speed;
+      return false;
+    }
 
-    if (letter == index) {
+    // Check end of animation for follow up animation and state changes.
+    switch (glyph.state) {
+      case IDLE_FLICKER_ON:
+        setColor(glyph, dark, fastSpeed);
+        glyph.state = IDLE_FLICKER_OFF;
+        return false;
+      case SIGNAL_FLASH:
+        setColor(glyph, dark, fastSpeed);
+        glyph.state = RESET_TO_DARK;
+        return false;
+      case CORRECT_PUSH:
+      case INCORRECT_PUSH:
+        setColor(glyph, dark, slowSpeed);
+        glyph.state = RESET_TO_DARK;
+        return false;
+      default:
+        glyph.state = IDLE_DARK;
+        return true;
+    }
+  }
+
+  // Check for glyph press.
+  if ((currentState == WAITING_FOR_PRESS) && isGlyphPressed(glyphId)) {
+    // Get the index for the current currect push.
+    int letter = answerSequence[revealedIndex] - 65;
+
+    if (letter == glyphId) {
       // Correct letter press!
-      if (--glyph.remainingLetterCount < 1) {
-        glyph.state = LOCK_COMPLETE;
-      } else {
-        glyph.state = LOCK_SUBSEQUENT;
-      }
+      glyph.state = CORRECT_PUSH;
       setColor(glyph, good_green, fastSpeed);
-      currentLetter++;
+      char nextCharacter = answerSequence[++pressIndex];
+      if (nextCharacter == '\0') {
+        // The players have entered the full sequence.
+        moveProgramState(PUZZLE_SOLVED);
+      } else if (pressIndex >= revealedIndex) {
+        // The players have pressed everything in the revealed sequence.
+        // Show the next part of the sequence.
+        revealedIndex++;
+        moveProgramState(SHOW_SEQUENCE);
+      }
+      return false;
     } else {
       // Incorrect letter press.
       glyph.state = INCORRECT_PUSH;
       setColor(glyph, wrong_red, fastSpeed);
+      moveProgramState(SHOW_SEQUENCE);
+      return false;
     }
   }
 
-  if (glyph.keyframe < 255) {
-    // Animate the glyph's LED.
-    byte speed = glyph.speed;
-
-    bool finished = true;
-    finished = finished && adjustColorComponent(glyph.currentColor.red, glyph.toColor.red, speed);
-    finished = finished && adjustColorComponent(glyph.currentColor.green, glyph.toColor.green, speed);
-    finished = finished && adjustColorComponent(glyph.currentColor.blue, glyph.toColor.blue, speed);
-
-    // Check for finished animation.
-    if (finished || (255 - glyph.keyframe) < speed) {
-      glyph.keyframe = 255;
-    } else {
-      glyph.keyframe += speed;
-    }
-    syncColor(index, glyph);
-  } else {
-    // We're at the end of an animation cycle.
-    // Check for transitions to follow-up or "resting" glyph states.
-    switch (state) {
-      case IDLE_FLICKER_ON:
-        glyph.state = IDLE_FLICKER_OFF;
-        setColor(glyph, dark, slowSpeed);
-        break;
-      case IDLE_FLICKER_OFF:
-        glyph.state = IDLE_DARK;
-        break;
-      case INCORRECT_PUSH:
-        glyph.state = WAITING_FOR_PUSH;
-        setColor(glyph, dark, slowSpeed);
-        break;
-      case CORRECT_PUSH:
-        if (glyph.remainingLetterCount < 1) {
-          glyph.state = LOCK_COMPLETE;
-        } else {
-          glyph.state = LOCK_SUBSEQUENT;
-        }
-        break;
-      case TRANSFER_TO_CRYSTAL:
-        glyph.state = RESET_TO_DARK;
-        setColor(glyph, dark, fastSpeed);
-        break;
-      case RESET_TO_DARK:
-        glyph.state = IDLE_DARK;
-        break;
-    }
-  }
+  // Nothing to do on this glyph.
+  return true;
 }
 
 /*
@@ -333,10 +365,46 @@ bool isGlyphPressed(int index) {
 }
 
 /*
+Clear the colors on all LEDs.
+*/
+void clearGlyphs() {
+  for (int i = 0; i < GLYPH_COUNT; i++) {
+    Glyph &glyph = glyphs[i];
+    setColor(glyph, dark, fastSpeed);
+  }
+}
+
+/*
+Flash all the glyphs green, to signal puzzle solved.
+*/
+void flashSuccessful() {
+  for (int i = 0; i < GLYPH_COUNT; i++) {
+    Glyph &glyph = glyphs[i];
+    setColor(glyph, good_green, fastSpeed);
+    glyph.state = CORRECT_PUSH;
+  }
+}
+
+/*
+Queue up an animation on a glyph, to the destination color at the given speed.
+*/
+void setColor(Glyph &glyph, Color color, byte speed) {
+  glyph.toColor.red = color.red;
+  glyph.toColor.green = color.green;
+  glyph.toColor.blue = color.blue;
+  if (speed < 1) {
+    glyph.speed = 1;
+  } else {
+    glyph.speed = speed;
+  }
+  glyph.keyframe = 0;
+}
+
+/*
 Sync the color, i.e. push it via the NeoPixel library. All of the necessary
 data is carried on the Glyph state object.
 */
-void syncColor(int index, Glyph glyph) {
+void syncColor(int index, Glyph &glyph) {
   Color color = glyph.currentColor;
   leds.setPixelColor(index, color.red, color.blue, color.green);
 }
@@ -345,7 +413,7 @@ void syncColor(int index, Glyph glyph) {
 Adjust one of the color components based on speed value. Returns true if the
 value to end at has been or will be reached.
 */
-bool adjustColorComponent(byte& currentValue, byte toValue, byte speed) {
+bool adjustColorComponent(byte &currentValue, byte toValue, byte speed) {
   if (currentValue == toValue) {
     // No change.
     return true;
@@ -368,17 +436,4 @@ bool adjustColorComponent(byte& currentValue, byte toValue, byte speed) {
     }
   }
   return currentValue == toValue;
-}
-
-/*
-Set a new color for the glyph, which sets all the values for the animation.
-*/
-void setColor(Glyph glyph, Color toColor, byte speed) {
-  glyph.toColor = toColor;
-  if (speed < 1) {
-    glyph.speed = 1;
-  } else {
-    glyph.speed = speed;
-  }
-  glyph.keyframe = 0;
 }
